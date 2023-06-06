5ac4d0f591299036cfaf9b925ed8b3c1227adbcb
nullptr-rs <nullptr.rs@gmail.com>
2023-06-06 09:12:13 +0200

    :tube: Remove docker tests

--- docker/src/test/java/io/github/nullptr/tools/docker/tests/helper/ContainerConsumer.java
+++
@@ -1,9 +0,0 @@
-package io.github.nullptr.tools.docker.tests.helper;
-
-import com.github.dockerjava.api.command.CreateContainerResponse;
-
-@FunctionalInterface
-public interface ContainerConsumer {
-
-    void accept(CreateContainerResponse container);
-}
--- docker/src/test/java/io/github/nullptr/tools/docker/tests/helper/TestContainerConsumer.java
+++
@@ -1,9 +0,0 @@
-package io.github.nullptr.tools.docker.tests.helper;
-
-import org.testcontainers.containers.GenericContainer;
-
-@FunctionalInterface
-public interface TestContainerConsumer {
-
-    void accept(GenericContainer<?> container);
-}
